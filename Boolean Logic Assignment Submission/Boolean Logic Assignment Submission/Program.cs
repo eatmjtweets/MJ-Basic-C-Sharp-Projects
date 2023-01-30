52acc99e595957b5f6925d2b0a2c95f98d8da664
eatmjtweets <111845489+eatmjtweets@users.noreply.github.com>
2023-01-29 17:58:10 -0700

    Add files via upload

---
+++ Boolean Logic Assignment Submission/Boolean Logic Assignment Submission/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Boolean_Logic_Assignment_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Approval program for car insurance");
+
+
+
+            //Applicant's Age
+            int applicantAge = 15;
+            Console.WriteLine("What is your age?");
+            bool ageResult = (applicantAge >= 15);
+            Console.ReadLine();
+
+
+
+            //Bool Applicant's DUI
+            Console.WriteLine("Have you ever had a DUI?");
+            bool applicantWithDui = false;
+            Console.ReadLine();
+
+
+
+            //Applicant's Speeding ticket
+            int applicantTicket = 3;
+            Console.WriteLine("How many speeding tickets do you have?");
+            bool ticketResult = (applicantTicket <= 3);
+            Console.ReadLine();
+
+
+            //Bool Qualified
+            Console.WriteLine("Qualified?:");
+            bool qualificationResult = (applicantAge >= 15 && applicantWithDui != true && applicantTicket <= 3);
+            Console.WriteLine(qualificationResult);
+            Console.ReadLine();
+        }
+    }
+}
