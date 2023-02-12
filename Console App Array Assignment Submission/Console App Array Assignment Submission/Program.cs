87938f32fede90d5e65a5752bcfe2675181da04c
eatmjtweets <111845489+eatmjtweets@users.noreply.github.com>
2023-02-11 21:04:20 -0700

    Add files via upload
    I left a note from Line 19.

---
+++ Console App Array Assignment Submission/Console App Array Assignment Submission/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+    static void Main()
+    {
+        Console.WriteLine("Chi would ask help again to? Choices are: \n Choose a number between 0 - 5.");
+
+        string[] nameArray = new string[5];
+
+        nameArray[0] = "It's Lyka";
+        nameArray[1] = "It's Eliza";
+        nameArray[2] = "It's Kim";
+        nameArray[3] = "It's MJ";
+        nameArray[4] = "It's ManPreet";
+        nameArray[5] = "It's Tej";
+
+        //HOW DO I PRINT THE RESULT OF USER'S INPUT?
+        Console.WriteLine(nameArray[5]);
+
+
+
+
+    }
+}
+
