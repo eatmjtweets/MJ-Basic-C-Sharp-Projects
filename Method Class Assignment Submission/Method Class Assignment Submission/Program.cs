33cb4b68daf59f44fe9215c4b5a1d348f5e10845
eatmjtweets <111845489+eatmjtweets@users.noreply.github.com>
2023-02-28 13:07:42 -0700

    Add files via upload

---
+++ Method Class Assignment Submission/Method Class Assignment Submission/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Method_Class_Assignment_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add two numbers: ");
+
+        }
+    }
+}
---
+++ Method Class Assignment Submission/Method Class Assignment Submission/math operation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Class_Assignment_Submission
+{
+    public class math_operation
+    {
+        public  
+    }
+}
