97067858a1fa4ecd6b3ba1e61acfb882ffbcf246
eatmjtweets <111845489+eatmjtweets@users.noreply.github.com>
2023-02-28 15:39:36 -0700

    Add files via upload

--- Method Class Assignment Submission/Method Class Assignment Submission/Program.cs
+++ Method Class Assignment Submission/Method Class Assignment Submission/Program.cs
@@ -6,7 +6,23 @@ namespace Method_Class_Assignment_Submission
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Add two numbers: ");
+            Console.WriteLine("Multiply two numbers: ");
+
+
+        }
+        public void MathOperations()
+        {
+            Console.WriteLine(num1);
+
+            Console.WriteLine("Multiplied by: ");
+
+            Console.WriteLine(num2);
+
+            Console.WriteLine("The product is: " + num3);
+
+
+
+
 
         }
     }
--- Method Class Assignment Submission/Method Class Assignment Submission/math operation.cs
+++ Method Class Assignment Submission/Method Class Assignment Submission/math operation.cs
@@ -6,6 +6,15 @@ namespace Method_Class_Assignment_Submission
 {
     public class math_operation
     {
-        public  
+        public void MathOperation(int num1, int num2, int num3)
+        {
+            num1 = 5;
+            num2 = 10;
+            num3 = num1 * num2;
+        }
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int num3 { get; set; }
+
     }
 }
